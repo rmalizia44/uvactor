54fca75da842eda14d179a9d49de94ad7a545c38
rmalizia44 <rmalizia44@gmail.com>
2022-09-09 19:41:20 -0300

    Add Reactor make function

--- include/reactor.hpp
+++ include/reactor.hpp
@@ -10,6 +10,11 @@ public:
 	using ActorPtr = Actor::SharedPtr;
 	using SelfPtr = ActorSelf::SharedPtr;
 	
+	template<typename T, typename... Ts>
+	static std::unique_ptr<Reactor> make(Ts&&... ts) {
+		return std::unique_ptr<Reactor>(new T(std::forward<Ts>(ts)...));
+	}
+	
 	virtual ~Reactor() = default;
 	virtual void dump(Writer& writer) const = 0;
 	virtual void react(const EventPtr& event, uint64_t timestamp) = 0;
